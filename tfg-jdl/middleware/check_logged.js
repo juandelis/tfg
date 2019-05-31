2957f42184879aa5ead5876fd168996c9f3b5313
Juan de Lis <juanlis96@hotmail.com>
2019-05-31 14:36:03 +0200

    limpieza

--- tfg-jdl/middleware/check_logged.js
+++
@@ -1,7 +0,0 @@
-export default function({ store, redirect, route }) {
-  const loginPage = '/login'
-  if (!store.getters['user/logged']) {
-    store.commit('user/setAfterLogin', route.path)
-    redirect(loginPage)
-  }
-}
