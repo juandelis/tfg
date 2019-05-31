5688365ca885f8417cef052067a8f20869ffa963
Juan de Lis <juanlis96@hotmail.com>
2019-05-31 13:07:04 +0200

    login redirige a logged, que permite volver a donde estabas

---
+++ tfg-jdl/pages/logged.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Login realizado correctamente...
+    </v-card-title>
+    <v-btn @click="back()">
+      VOLVER
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+  methods: {
+    back() {
+      // this.$router.push(this.afterLogin)
+      this.$router.go(-2) // Vuelve a la página donde estabas antes del login
+    }
+  }
+}
+</script>
+
+<style scoped></style>
