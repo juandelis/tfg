0aae52e260d68c5dc7dec0524e27f9c875050747
Juan de Lis <juanlis96@hotmail.com>
2019-07-17 15:01:50 +0200

    muestra usuarios en la base de datos

--- tfg-jdl/pages/users/index.vue
+++ tfg-jdl/pages/users/index.vue
@@ -12,30 +12,69 @@
 
       <br /><br />
 
-      <v-card>
+      <v-card ml-4>
         <br />
         <h1 align="center">USUARIOS</h1>
         <br />
 
         <hr />
         <br />
+
+        <h3 align="center">USERS: &nbsp; {{ users }}</h3>
+
+        <br />
+        <hr />
+        <br />
+
+        <v-list v-for="(user, i) in users" :key="i">
+          <div v-if="user.followed">
+            {{ i + 1 }} - NOMBRE: {{ user.name }} -- CORREO: {{ user.email }}
+            <v-btn color="red" flat nuxt @click="unfollow()">
+              DEJAR DE SEGUIR
+            </v-btn>
+          </div>
+          <div v-else>
+            {{ i + 1 }} - NOMBRE: {{ user.name }} -- CORREO: {{ user.email }}
+            <v-btn color="green" flat @click="follow()">
+              SEGUIR
+            </v-btn>
+          </div>
+        </v-list>
+        <br />
       </v-card>
+      <br />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import { mapState } from 'vuex'
-// import { storage } from '~/services/fireinit'
+import { db } from '~/services/fireinit'
 
 export default {
-  data: () => ({}),
+  data() {
+    return {
+      users: []
+    }
+  },
   middleware: 'autenticado',
   computed: {
     ...mapState('user', ['user'])
   },
+  mounted: function() {
+    this.getUsers()
+  },
   methods: {
-    // ...mapActions('user', ['updateUserImage', 'test']),
+    async getUsers() {
+      const usersSnapshot = await db.collection('accounts').get()
+      usersSnapshot.forEach(userDoc => {
+        const user = userDoc.data()
+        console.log('userDoc: ' + userDoc.id)
+        // TODO mirar si le sigue o no -> followed: true/false
+        this.users.push({ name: user.name, email: user.email, followed: true })
+        // console.log('Usuario: ' + user.email)
+      })
+    }
   }
 }
 </script>
