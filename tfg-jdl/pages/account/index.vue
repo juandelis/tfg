23f22349d3bd36198db6801f461fcbc069f54aa2
Juan de Lis <juanlis96@hotmail.com>
2019-08-07 15:37:58 +0200

    añadido seguidos en "Mi perfil"

--- tfg-jdl/pages/account/index.vue
+++ tfg-jdl/pages/account/index.vue
@@ -67,24 +67,32 @@
         </div>
         <br />
       </v-card>
+
+      <br /><br />
+
+      <v-card>
+        <br />
+        <h1 align="center">SEGUIDOS ( {{ followed.length }} )</h1>
+        <hr />
+        <br />
+        <div v-for="(user, i) in followed" :key="i">
+          {{ i + 1 }} -&nbsp; {{ user.name }} &nbsp; --- &nbsp; {{ user.email }}
+        </div>
+        <br />
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex'
-import { db, getCurrentUser, storage } from '~/services/fireinit'
+import { db, storage } from '~/services/fireinit'
 
 export default {
-  /* data: () => ({
-    imageURL: '',
-    imageDownloadURL:
-      'https://firebasestorage.googleapis.com/v0/b/tfg-jdl.appspot.com/o/profileImages%2Fjuan.jpg?alt=media&token=5d5e7241-a753-46ed-9fc3-848627e727d1',
-    image: null
-  }), */
   data() {
     return {
-      followers: []
+      followers: [],
+      followed: []
     }
   },
   middleware: 'autenticado',
@@ -96,30 +104,38 @@ export default {
   },
   methods: {
     ...mapActions('user', ['updateUserImage', 'test']),
+
     async getUsers() {
       const usersSnapshot = await db.collection('accounts').get()
-      const userLogged = await getCurrentUser()
-      const userFollowers = this.user.followers
       usersSnapshot.forEach(eachUserDoc => {
         const eachUserData = eachUserDoc.data()
-        if (
-          eachUserDoc.id !== userLogged.uid &&
-          userFollowers.includes(eachUserDoc.id)
-        ) {
-          this.followers.push({
-            id: eachUserDoc.id,
-            name: eachUserData.name,
-            email: eachUserData.email
-          })
+        if (eachUserDoc.id !== this.user.uid) {
+          if (this.user.followed.includes(eachUserDoc.id)) {
+            this.followed.push({
+              id: eachUserDoc.id,
+              name: eachUserData.name,
+              email: eachUserData.email
+            })
+          }
+          if (this.user.followers.includes(eachUserDoc.id)) {
+            this.followers.push({
+              id: eachUserDoc.id,
+              name: eachUserData.name,
+              email: eachUserData.email
+            })
+          }
         }
       })
     },
+
     click_fileInput() {
       this.$refs.fileInput.click()
     },
+
     updateImage(newImage) {
       this.updateUserImage(newImage)
     },
+
     async onFileChange(event) {
       const files = event.target.files
       const newImage = files[0]
@@ -135,48 +151,6 @@ export default {
       const downloadURL = await snapshot.ref.getDownloadURL()
       console.log('File available at', downloadURL)
       this.updateUserImage(downloadURL)
-
-      /* storageRef.put(newImage).then(function(uploadTask) {
-        console.log(
-          'File available att' + uploadTask.snapshot.ref.getDownloadURL()
-        )
-        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-          console.log('File available at', downloadURL)
-          // updateImage(downloadURL)
-        })
-      })
-
-            const uploadTask = storageRef.put(newImage)
-
-      uploadTask.on(
-        'state_changed',
-        function(snapshot) {},
-        function(error) {
-          console.error(error)
-        },
-        function() {
-          uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-            console.log('File available at', downloadURL)
-            // updateImage(downloadURL)
-          })
-          this.imageURL = 'v.png'
-          console.log('image ' + this.imageURL)
-          // this.updateUserImage('v.png')
-        }
-      ) */
-
-      /* const fileReader = new FileReader()
-      fileReader.addEventListener('load', () => {
-        this.imageURL = fileReader.result
-        // this.updateImage('images/GamersCrew.png')
-        console.log('Imagen: ' + this.$store.state.user.image)
-        // this.$router.push('/')
-        // this.$store.commit('updateImage', fileReader.result)
-        // console.log('imageURL: ' + this.imageURL)
-      })
-
-      fileReader.readAsDataURL(files[0])
-      this.image = files[0] */
     }
   }
 }
