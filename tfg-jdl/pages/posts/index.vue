adea29fbbe76250f353a827e5988d329eba5bc7e
Juan de Lis <juanlis96@hotmail.com>
2020-02-13 20:47:32 +0100

    sincronizacion datos tiempo real (onsnapshot) y filtro busqueda fecha

--- tfg-jdl/pages/posts/index.vue
+++ tfg-jdl/pages/posts/index.vue
@@ -1,100 +1,149 @@
 <template>
   <v-layout justify-center>
-    <v-flex text-xs-center xs12 sm9 md7>
+    <v-flex text-xs-center xs12 sm9 md6>
       <v-card>
         <br />
         <h1 align="center">PUBLICACIONES</h1>
-
         <br />
         <hr />
         <br />
-
         <p>
-          <label class="labelForm" for="tittle"> Titulo: </label>
-          <input v-model="tittle" type="search" name="titulo" />
-        </p>
-
-        <p>
+          <label class="labelForm" for="creator"> Autor: </label> &nbsp;
+          <input v-model="creator" type="search" name="creator" size="40" />
+          <br /><br />
+          <label class="labelForm" for="date"> Fecha: </label> &nbsp;
+          <input v-model="date" type="date" name="date" />
+          <br /><br />
           <v-btn @click="search()">
             BUSCAR
           </v-btn>
         </p>
-
-        <hr />
         <br />
-
-        <div v-for="(post, i) in posts" :key="i">
-          <v-btn
-            flat
-            color="white"
-            left
-            round
-            @click="showPost(post.uid, post.creatorUid)"
-          >
-            {{ i + 1 }} - &nbsp; {{ post.tittle }} &nbsp; ---&nbsp;
-            {{ post.date }}
-          </v-btn>
-          <br /><br />
-        </div>
       </v-card>
+
+      <br />
+
+      <div v-for="(post, i) in posts" :key="i">
+        <v-card dark>
+          <v-card-title>
+            <v-btn flat round @click="showUser(post.creatorUid)">
+              <v-icon>account_circle</v-icon>&nbsp;
+              <span class="headline font-weight-bold">
+                {{ post.creatorName }}
+              </span>
+            </v-btn>
+            <v-spacer />
+            {{ post.date }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          </v-card-title>
+          {{ post.body }}
+          <br />
+          <v-card-title>
+            <v-spacer />
+            <v-card color="">
+              <v-btn icon flat @click="showUser(post.creatorUid)">
+                <v-icon>thumb_down</v-icon>
+              </v-btn>
+              <span>256</span>
+              <v-btn icon flat @click="showUser(post.creatorUid)">
+                <v-icon>thumb_up</v-icon>
+              </v-btn>
+            </v-card>
+            <v-spacer />
+          </v-card-title>
+        </v-card>
+        <br />
+      </div>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import { db } from '~/services/fireinit'
-// import { storage } from '~/services/fireinit'
 
 export default {
   data: () => ({
     posts: [],
-    tittle: ''
+    creator: '',
+    date: '',
+    unsubscribe: null
   }),
   middleware: 'autenticado',
   computed: {
     ...mapState('user', ['user'])
   },
   mounted: async function() {
-    this.posts = []
-    const postsSnapshot = await db.collection('posts').get()
-    // const userLogged = this.user
-    postsSnapshot.forEach(postDoc => {
-      const postData = postDoc.data()
-      this.posts.push({
-        uid: postDoc.id,
-        tittle: postData.tittle,
-        date: postData.date.toDate().toLocaleDateString('es-ES'),
-        creatorUid: postData.creatorUid,
-        creatorName: postData.creatorName,
-        body: postData.body
-      })
+    // TODO missings
+
+    // TODO onSnapShot para el user
+    // Se puede también escuchar a un documento concreto
+
+    const postsCollection = await db.collection('posts').orderBy('date', 'desc')
+
+    console.log('Start listening for changes')
+    // this.unsubscribe guarda la funcion para dejar de escuchar (se invocará en beforeDestroy)
+    this.unsubscribe = postsCollection.onSnapshot(postsSnapshot => {
+      // TODO Actualizar solo los cambios, no quitar todo y añadir todo de nuevo
+      this.getPosts(postsSnapshot)
     })
   },
+  beforeDestroy: function() {
+    // Limpiar posts
+    this.posts = []
+    // Dejar de escuchar
+    this.unsubscribe()
+    console.log('Stop listening for changes')
+  },
   methods: {
+    // TODO funciones de like y dislike (añadir id de usuario a lista de likes o dislikes)
+    ...mapActions('user', ['showUser']),
+
     async search() {
       this.posts = []
-      const postsSnapshot = await db.collection('posts').get()
-      postsSnapshot.forEach(postDoc => {
+      const postsColllection = await db
+        .collection('posts')
+        .orderBy('date', 'desc')
+        .get()
+
+      postsColllection.forEach(postDoc => {
         const postData = postDoc.data()
-        if (postData.tittle.toLowerCase().includes(this.tittle.toLowerCase())) {
+        if (
+          postData.creatorName
+            .toLowerCase()
+            .includes(this.creator.toLowerCase()) &&
+          (this.date === ''
+            ? true
+            : postData.date
+                .toDate()
+                .toISOString()
+                .split('T')[0] === this.date)
+        ) {
           this.posts.push({
             uid: postDoc.id,
             tittle: postData.tittle,
             date: postData.date.toDate().toLocaleDateString('es-ES'),
-            creator: postData.creator,
+            creatorUid: postData.creatorUid,
+            creatorName: postData.creatorName,
             body: postData.body
           })
         }
       })
     },
 
-    showPost(idPostToShow, idCreator) {
-      if (idCreator === this.user.uid) {
-        this.$router.push('/myposts/' + idPostToShow) // TODO MYPOSTS
-      } else {
-        this.$router.push('/posts/' + idPostToShow)
-      }
+    getPosts(postsSnapshot) {
+      // TODO Actualizar solo los cambios, no quitar todo y añadir todo de nuevo
+      this.posts = []
+      postsSnapshot.forEach(postDoc => {
+        const postData = postDoc.data()
+        this.posts.push({
+          uid: postDoc.id,
+          tittle: postData.tittle,
+          date: postData.date.toDate().toLocaleDateString('es-ES'),
+          creatorUid: postData.creatorUid,
+          creatorName: postData.creatorName,
+          body: postData.body
+        })
+      })
     }
     /*,
     getDateWithFormat(date) {
