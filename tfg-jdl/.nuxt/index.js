967accc10109b0195f367459a825478b98b01727
Juan de Lis <juanlis96@hotmail.com>
2020-04-26 20:20:16 +0200

    cosas de nuxt

--- tfg-jdl/.nuxt/index.js
+++ tfg-jdl/.nuxt/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Meta from 'vue-meta'
+import ClientOnly from 'vue-client-only'
+import NoSsr from 'vue-no-ssr'
 import { createRouter } from './router.js'
-import NoSsr from './components/no-ssr.js'
 import NuxtChild from './components/nuxt-child.js'
 import NuxtError from './components/nuxt-error.vue'
 import Nuxt from './components/nuxt.js'
@@ -13,8 +14,21 @@ import { createStore } from './store.js'
 
 import nuxt_plugin_vuetify_e5914fcc from 'nuxt_plugin_vuetify_e5914fcc' // Source: ..\\plugins\\vuetify (mode: 'all')
 
-// Component: <NoSsr>
-Vue.component(NoSsr.name, NoSsr)
+// Component: <ClientOnly>
+Vue.component(ClientOnly.name, ClientOnly)
+
+// TODO: Remove in Nuxt 3: <NoSsr>
+Vue.component(NoSsr.name, {
+  ...NoSsr,
+  render (h, ctx) {
+    if (process.client && !NoSsr._warned) {
+      NoSsr._warned = true
+
+      console.warn('<no-ssr> has been deprecated and will be removed in Nuxt 3, please use <client-only> instead')
+    }
+    return NoSsr.render(h, ctx)
+  }
+})
 
 // Component: <NuxtChild>
 Vue.component(NuxtChild.name, NuxtChild)
@@ -22,20 +36,14 @@ Vue.component('NChild', NuxtChild)
 
 // Component NuxtLink is imported in server.js or client.js
 
-// Component: <Nuxt>`
+// Component: <Nuxt>
 Vue.component(Nuxt.name, Nuxt)
 
-// vue-meta configuration
-Vue.use(Meta, {
-  keyName: 'head', // the component option name that vue-meta looks for meta info on.
-  attribute: 'data-n-head', // the attribute name vue-meta adds to the tags it observes
-  ssrAttribute: 'data-n-head-ssr', // the attribute name that lets vue-meta know that meta info has already been server-rendered
-  tagIDKeyName: 'hid' // the property name that vue-meta uses to determine whether to overwrite or append a tag
-})
+Vue.use(Meta, {"keyName":"head","attribute":"data-n-head","ssrAttribute":"data-n-head-ssr","tagIDKeyName":"hid"})
 
 const defaultTransition = {"name":"page","mode":"out-in","appear":true,"appearClass":"appear","appearActiveClass":"appear-active","appearToClass":"appear-to"}
 
-async function createApp(ssrContext) {
+async function createApp (ssrContext) {
   const router = await createRouter(ssrContext)
 
   const store = createStore(ssrContext)
@@ -47,14 +55,16 @@ async function createApp(ssrContext) {
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    router,
+    head: {"title":"tfg-jdl","meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":"Nuxt+Firebase app for degree final proyect"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"},{"rel":"stylesheet","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Roboto:300,400,500,700|Material+Icons"}],"style":[],"script":[]},
+
     store,
+    router,
     nuxt: {
       defaultTransition,
-      transitions: [ defaultTransition ],
-      setTransitions(transitions) {
+      transitions: [defaultTransition],
+      setTransitions (transitions) {
         if (!Array.isArray(transitions)) {
-          transitions = [ transitions ]
+          transitions = [transitions]
         }
         transitions = transitions.map((transition) => {
           if (!transition) {
@@ -69,17 +79,23 @@ async function createApp(ssrContext) {
         this.$options.nuxt.transitions = transitions
         return transitions
       },
+
       err: null,
       dateErr: null,
-      error(err) {
+      error (err) {
         err = err || null
-        app.context._errored = !!err
+        app.context._errored = Boolean(err)
         err = err ? normalizeError(err) : null
-        const nuxt = this.nuxt || this.$options.nuxt
+        let nuxt = app.nuxt // to work with @vue/composition-api, see https://github.com/nuxt/nuxt.js/issues/6517#issuecomment-573280207
+        if (this) {
+          nuxt = this.nuxt || this.$options.nuxt
+        }
         nuxt.dateErr = Date.now()
         nuxt.err = err
         // Used in src/server.js
-        if (ssrContext) ssrContext.nuxt.error = err
+        if (ssrContext) {
+          ssrContext.nuxt.error = err
+        }
         return err
       }
     },
@@ -95,25 +111,31 @@ async function createApp(ssrContext) {
   if (ssrContext) {
     route = router.resolve(ssrContext.url).route
   } else {
-    const path = getLocation(router.options.base)
+    const path = getLocation(router.options.base, router.options.mode)
     route = router.resolve(path).route
   }
 
   // Set context to app.context
   await setContext(app, {
+    store,
     route,
     next,
     error: app.nuxt.error.bind(app),
-    store,
     payload: ssrContext ? ssrContext.payload : undefined,
     req: ssrContext ? ssrContext.req : undefined,
     res: ssrContext ? ssrContext.res : undefined,
-    beforeRenderFns: ssrContext ? ssrContext.beforeRenderFns : undefined
+    beforeRenderFns: ssrContext ? ssrContext.beforeRenderFns : undefined,
+    ssrContext
   })
 
   const inject = function (key, value) {
-    if (!key) throw new Error('inject(key, value) has no key provided')
-    if (typeof value === 'undefined') throw new Error('inject(key, value) has no value provided')
+    if (!key) {
+      throw new Error('inject(key, value) has no key provided')
+    }
+    if (value === undefined) {
+      throw new Error(`inject('${key}', value) has no value provided`)
+    }
+
     key = '$' + key
     // Add into app
     app[key] = value
@@ -123,13 +145,15 @@ async function createApp(ssrContext) {
 
     // Check if plugin not already installed
     const installKey = '__nuxt_' + key + '_installed__'
-    if (Vue[installKey]) return
+    if (Vue[installKey]) {
+      return
+    }
     Vue[installKey] = true
     // Call Vue.use() to install the plugin into vm
     Vue.use(() => {
-      if (!Vue.prototype.hasOwnProperty(key)) {
+      if (!Object.prototype.hasOwnProperty.call(Vue, key)) {
         Object.defineProperty(Vue.prototype, key, {
-          get() {
+          get () {
             return this.$root.$options[key]
           }
         })
@@ -146,7 +170,9 @@ async function createApp(ssrContext) {
 
   // Plugin execution
 
-  if (typeof nuxt_plugin_vuetify_e5914fcc === 'function') await nuxt_plugin_vuetify_e5914fcc(app.context, inject)
+  if (typeof nuxt_plugin_vuetify_e5914fcc === 'function') {
+    await nuxt_plugin_vuetify_e5914fcc(app.context, inject)
+  }
 
   // If server-side, wait for async component to be resolved first
   if (process.server && ssrContext && ssrContext.url) {
@@ -166,8 +192,8 @@ async function createApp(ssrContext) {
   }
 
   return {
-    app,
     store,
+    app,
     router
   }
 }
--- tfg-jdl/.nuxt/router.js
+++ tfg-jdl/.nuxt/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
 
 const _47e4f73a = () => interopDefault(import('..\\pages\\account\\index.vue' /* webpackChunkName: "pages_account_index" */))
 const _743269d5 = () => interopDefault(import('..\\pages\\index2.vue' /* webpackChunkName: "pages_index2" */))
@@ -18,142 +19,87 @@ const _ebdb4708 = () => interopDefault(import('..\\pages\\posts\\_id.vue' /* web
 const _17f2f5e7 = () => interopDefault(import('..\\pages\\users\\_id.vue' /* webpackChunkName: "pages_users__id" */))
 const _dfbc35a6 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
 Vue.use(Router)
 
-if (process.client) {
-  if ('scrollRestoration' in window.history) {
-    window.history.scrollRestoration = 'manual'
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
 
-    // reset scrollRestoration to auto when leaving page, allowing page reload
-    // and back-navigation from other pages to use the browser to restore the
-    // scrolling position.
-    window.addEventListener('beforeunload', () => {
-      window.history.scrollRestoration = 'auto'
-    })
+  routes: [{
+    path: "/account",
+    component: _47e4f73a,
+    name: "account"
+  }, {
+    path: "/index2",
+    component: _743269d5,
+    name: "index2"
+  }, {
+    path: "/inspire",
+    component: _e12d7316,
+    name: "inspire"
+  }, {
+    path: "/login",
+    component: _07e09084,
+    name: "login"
+  }, {
+    path: "/posts",
+    component: _7ed62514,
+    name: "posts"
+  }, {
+    path: "/pruebas",
+    component: _d3b9b346,
+    name: "pruebas"
+  }, {
+    path: "/users",
+    component: _16f60fbf,
+    name: "users"
+  }, {
+    path: "/account/edit",
+    component: _a4c3465c,
+    name: "account-edit"
+  }, {
+    path: "/account/password",
+    component: _7f01e0e3,
+    name: "account-password"
+  }, {
+    path: "/login/password",
+    component: _8bc9bc42,
+    name: "login-password"
+  }, {
+    path: "/posts/create",
+    component: _f8ba6fac,
+    name: "posts-create"
+  }, {
+    path: "/posts/myposts",
+    component: _6546a0e9,
+    name: "posts-myposts"
+  }, {
+    path: "/posts/:id",
+    component: _ebdb4708,
+    name: "posts-id"
+  }, {
+    path: "/users/:id",
+    component: _17f2f5e7,
+    name: "users-id"
+  }, {
+    path: "/",
+    component: _dfbc35a6,
+    name: "index"
+  }],
 
-    // Setting scrollRestoration to manual again when returning to this page.
-    window.addEventListener('load', () => {
-      window.history.scrollRestoration = 'manual'
-    })
-  }
+  fallback: false
 }
-const scrollBehavior = function (to, from, savedPosition) {
-  // if the returned position is falsy or an empty object,
-  // will retain current scroll position.
-  let position = false
 
-  // if no children detected and scrollToTop is not explicitly disabled
-  if (
-    to.matched.length < 2 &&
-    to.matched.every(r => r.components.default.options.scrollToTop !== false)
-  ) {
-    // scroll to the top of the page
-    position = { x: 0, y: 0 }
-  } else if (to.matched.some(r => r.components.default.options.scrollToTop)) {
-    // if one of the children has scrollToTop option set to true
-    position = { x: 0, y: 0 }
-  }
-
-  // savedPosition is only available for popstate navigations (back button)
-  if (savedPosition) {
-    position = savedPosition
-  }
-
-  return new Promise((resolve) => {
-    // wait for the out transition to complete (if necessary)
-    window.$nuxt.$once('triggerScroll', () => {
-      // coords will be used if no selector is provided,
-      // or if the selector didn't match any element.
-      if (to.hash) {
-        let hash = to.hash
-        // CSS.escape() is not supported with IE and Edge.
-        if (typeof window.CSS !== 'undefined' && typeof window.CSS.escape !== 'undefined') {
-          hash = '#' + window.CSS.escape(hash.substr(1))
-        }
-        try {
-          if (document.querySelector(hash)) {
-            // scroll to anchor by returning the selector
-            position = { selector: hash }
-          }
-        } catch (e) {
-          console.warn('Failed to save scroll position. Please add CSS.escape() polyfill (https://github.com/mathiasbynens/CSS.escape).')
-        }
-      }
-      resolve(position)
-    })
-  })
-}
-
-export function createRouter() {
-  return new Router({
-    mode: 'history',
-    base: '/',
-    linkActiveClass: 'nuxt-link-active',
-    linkExactActiveClass: 'nuxt-link-exact-active',
-    scrollBehavior,
-
-    routes: [{
-      path: "/account",
-      component: _47e4f73a,
-      name: "account"
-    }, {
-      path: "/index2",
-      component: _743269d5,
-      name: "index2"
-    }, {
-      path: "/inspire",
-      component: _e12d7316,
-      name: "inspire"
-    }, {
-      path: "/login",
-      component: _07e09084,
-      name: "login"
-    }, {
-      path: "/posts",
-      component: _7ed62514,
-      name: "posts"
-    }, {
-      path: "/pruebas",
-      component: _d3b9b346,
-      name: "pruebas"
-    }, {
-      path: "/users",
-      component: _16f60fbf,
-      name: "users"
-    }, {
-      path: "/account/edit",
-      component: _a4c3465c,
-      name: "account-edit"
-    }, {
-      path: "/account/password",
-      component: _7f01e0e3,
-      name: "account-password"
-    }, {
-      path: "/login/password",
-      component: _8bc9bc42,
-      name: "login-password"
-    }, {
-      path: "/posts/create",
-      component: _f8ba6fac,
-      name: "posts-create"
-    }, {
-      path: "/posts/myposts",
-      component: _6546a0e9,
-      name: "posts-myposts"
-    }, {
-      path: "/posts/:id",
-      component: _ebdb4708,
-      name: "posts-id"
-    }, {
-      path: "/users/:id",
-      component: _17f2f5e7,
-      name: "users-id"
-    }, {
-      path: "/",
-      component: _dfbc35a6,
-      name: "index"
-    }],
-
-    fallback: false
-  })
+export function createRouter () {
+  return new Router(routerOptions)
 }
