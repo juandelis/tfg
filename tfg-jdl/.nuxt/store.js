c46e9c2a4321f1cc8e5f578a1fa29a126ece140d
Juan de Lis <juanlis96@hotmail.com>
2020-06-03 21:10:10 +0200

    ESLint --fix y firebase deploy

--- tfg-jdl/.nuxt/store.js
+++ tfg-jdl/.nuxt/store.js
@@ -10,10 +10,6 @@ let store = {};
 (function updateModules () {
   // If store is an exported method = classic mode (deprecated)
 
-  if (typeof store === 'function') {
-    return console.warn('Classic mode for store/ is deprecated and will be removed in Nuxt 3.')
-  }
-
   // Enforce store modules
   store.modules = store.modules || {}
 
@@ -24,22 +20,6 @@ let store = {};
   resolveStoreModules(require('..\\store\\userToShow.js'), 'userToShow.js')
 
   // If the environment supports hot reloading...
-
-  if (process.client && module.hot) {
-    // Whenever any Vuex module is updated...
-    module.hot.accept([
-      '..\\store\\myposts.js',
-      '..\\store\\posts.js',
-      '..\\store\\user.js',
-      '..\\store\\users.js',
-      '..\\store\\userToShow.js',
-    ], () => {
-      // Update `root.modules` with the latest definitions.
-      updateModules()
-      // Trigger a hot update in the store.
-      window.$nuxt.$store.hotUpdate(store)
-    })
-  }
 })()
 
 // createStore
--- tfg-jdl/functions/index.js
+++ tfg-jdl/functions/index.js
@@ -1,26 +1,50 @@
-const functions = require('firebase-functions');
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions')
 
-exports.updateUser = functions.firestore
-    .document('accounts/{userId}')
-    .onUpdate((change, context) => {
-      const newData = change.after.data();
-      const oldData = change.before.data();
+// The Firebase Admin SDK to access the Firebase Firestore Database.
+const admin = require('firebase-admin')
+//admin.initializeApp(FirebaseConfig);
+admin.initializeApp()
+const db = admin.firestore()
 
-      // Cambio en usuarios seguidos (array Following)
-      if(newData.following != oldData.following){
+exports.updateUserFollows = functions.firestore
+  .document('accounts/{userId}')
+  .onUpdate((change, context) => {
+    const user = context.params.userId
+    const newData = change.after.data()
+    const oldData = change.before.data()
 
-      }
+    // TEST
+    const docRef = db.doc('accounts/' + user).update({
+      //info: 'Me gusta Marta, es la mejor!!'
+      info: 'TEST'
+    })
 
+    // Changes in Following array
+    /*if(newData.following != oldData.following){
+        // Follows (In newData but not in oldData)
+        for (String userToFollow : newData.following){
+          if(!oldData.following.includes(userToFollow)){
+              // Add user to followers array of userToFollow
+              const docRef = await db.collection('accounts').doc(userToFollow)
+              docRef.update({
+                followers: firestore.FieldValue.arrayUnion(user)
+              })
+              console.log("CUCU");
+          }
+        }
+        // Unfollows (In oldData but not in newData)
+        for (String userToUnfollow : oldData.following){
+          if(!newData.following.includes(item)){
+              // Remove user from followers array of userToUnfollow
+              const docRef2 = await db.collection('accounts').doc(userToUnfollow)
+              docRef2.update({
+                followers: firestore.FieldValue.arrayRemove(user)
+              })
+              console.log("CHIQUITÍN");
+          }
+        }
+      }*/
 
-      // access a particular field as you would any JS property
-      const name = newValue.name;
-
-      // perform desired operations ...
-    });
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+    return null
+  })
