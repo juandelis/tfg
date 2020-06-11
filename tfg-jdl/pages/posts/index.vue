7a2cd0ea817eb0fee99773c07c082ff2fb26dc12
Juan de Lis <juanlis96@hotmail.com>
2020-06-12 01:54:51 +0200

    busqueda posts mas sencilla y cambio de estetica

--- tfg-jdl/pages/posts/index.vue
+++ tfg-jdl/pages/posts/index.vue
@@ -8,119 +8,96 @@
         <hr />
         <br />
         <p>
-          <label class="labelForm" for="creator"> Autor: </label> &nbsp;
-          <input
-            v-model="creator"
-            type="search"
-            name="creator"
-            size="40"
-            @input="searchPosts({ creator: creator, date: date, type: type })"
-          />
-          <br /><br />
-          <label class="labelForm" for="date"> Fecha: </label> &nbsp;
-          <input
-            v-model="date"
-            type="date"
-            name="date"
-            @input="searchPosts({ creator: creator, date: date, type: type })"
-          />
-          <br /><br />
-        </p>
-        <p>
           <input
             v-model="type"
             type="radio"
             value="all"
-            @input="searchPosts({ creator: creator, date: date, type: 'all' })"
+            @input="searchPosts({ date: date, type: 'all' })"
           />
           Todas &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <input
             v-model="type"
             type="radio"
             value="liked"
-            @input="
-              searchPosts({ creator: creator, date: date, type: 'liked' })
-            "
+            @input="searchPosts({ date: date, type: 'liked' })"
           />
           Me gustan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <input
             v-model="type"
             type="radio"
             value="disliked"
-            @input="
-              searchPosts({
-                creator: creator,
-                date: date,
-                type: 'disliked'
-              })
-            "
+            @input="searchPosts({ date: date, type: 'disliked' })"
           />
           No me gustan &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <input
             v-model="type"
             type="radio"
             value="notValued"
-            @input="
-              searchPosts({
-                creator: creator,
-                date: date,
-                type: 'notValued'
-              })
-            "
+            @input="searchPosts({ date: date, type: 'notValued' })"
           />
           Sin valorar
         </p>
+        <p>
+          <label for="date"> Fecha: </label> &nbsp;
+          <input
+            v-model="date"
+            type="date"
+            name="date"
+            @input="searchPosts({ date: date, type: type })"
+          />
+          <br />
+        </p>
         <!--<br /><br />
-          <v-btn @click="searchPosts({ creator: creator, date: date })">
+          <v-btn @click="searchPosts({ date: date })">
             BUSCAR
           </v-btn>-->
-        <br />
-      </v-card>
 
-      <br />
+        <hr />
 
-      <div v-for="(post, i) in posts" :key="i">
-        <v-card>
-          <v-card-title>
-            <v-btn flat round @click="showUser(post.creatorId)">
-              <v-icon>account_circle</v-icon>&nbsp;
-              <span class="headline font-weight-bold">
-                {{ post.creatorName }}
-              </span>
-            </v-btn>
-            <v-spacer />
-            {{ post.date }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </v-card-title>
-          <v-card-text>
-            {{ post.body }}
-          </v-card-text>
-          <v-card-title>
-            <v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer />
-            <v-card>
-              &nbsp;&nbsp;&nbsp;&nbsp; {{ post.num_likes }}
-              <v-btn v-if="!post.liked" icon @click="like(post.id)">
-                <v-icon>thumb_up</v-icon>
-              </v-btn>
-              <v-btn v-else color="#878787" icon @click="unlike(post.id)">
-                <v-icon>thumb_up</v-icon>
-              </v-btn>
-            </v-card>
-            <v-spacer />
-            <v-card>
-              &nbsp;&nbsp;&nbsp;&nbsp; {{ post.num_dislikes }}
-              <v-btn v-if="!post.disliked" icon @click="dislike(post.id)">
-                <v-icon>thumb_down</v-icon>
-              </v-btn>
-              <v-btn v-else color="#878787" icon @click="undislike(post.id)">
-                <v-icon>thumb_down</v-icon>
+        <div v-for="(post, i) in posts" :key="i">
+          <br />
+          <v-card elevation="10" color="#444444">
+            <v-card-title>
+              <v-btn flat round @click="showUser(post.creatorId)">
+                <v-icon>account_circle</v-icon>&nbsp;
+                <span class="headline font-weight-bold">
+                  {{ post.creatorName }}
+                </span>
               </v-btn>
               <v-spacer />
-            </v-card>
-            <v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer />
-          </v-card-title>
-        </v-card>
+              {{ post.date }}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </v-card-title>
+            <v-card-text>
+              {{ post.body }}
+            </v-card-text>
+            <v-card-title>
+              <v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer />
+              <v-card>
+                &nbsp;&nbsp;&nbsp;&nbsp; {{ post.num_likes }}
+                <v-btn v-if="!post.liked" icon @click="like(post.id)">
+                  <v-icon>thumb_up</v-icon>
+                </v-btn>
+                <v-btn v-else color="#878787" icon @click="unlike(post.id)">
+                  <v-icon>thumb_up</v-icon>
+                </v-btn>
+              </v-card>
+              <v-spacer />
+              <v-card>
+                &nbsp;&nbsp;&nbsp;&nbsp; {{ post.num_dislikes }}
+                <v-btn v-if="!post.disliked" icon @click="dislike(post.id)">
+                  <v-icon>thumb_down</v-icon>
+                </v-btn>
+                <v-btn v-else color="#878787" icon @click="undislike(post.id)">
+                  <v-icon>thumb_down</v-icon>
+                </v-btn>
+                <v-spacer />
+              </v-card>
+              <v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer /><v-spacer />
+            </v-card-title>
+          </v-card>
+        </div>
         <br />
-      </div>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
@@ -130,7 +107,8 @@ import { mapState, mapActions } from 'vuex'
 
 export default {
   data: () => ({
-    creator: '',
+    // TODO: dejar fecha de hoy
+    // date: new Date().toISOString().substr(0, 10),
     date: '',
     type: 'all'
   }),
@@ -144,7 +122,6 @@ export default {
 
   mounted: function() {
     this.startListeningToPosts({
-      creator: this.creator,
       date: this.date,
       type: this.type
     })
