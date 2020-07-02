65f4909ce2175361939fc64e3d5ae2d189fca95f
Juan de Lis <juanlis96@hotmail.com>
2020-07-02 22:09:50 +0200

    página de error con error 404 y genérico

---
+++ tfg-jdl/layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">
+      Ups... No hemos podido encontrar la página. <br />
+      (El típico error 404, vamos.)
+    </h1>
+    <h1 v-else>Ha ocurrido un error inesperado. Perdón.</h1>
+    <br /><br />
+
+    <v-btn nuxt to="/">Volver al inicio</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  layout: 'error', // you can set a custom layout for the error page
+}
+</script>
