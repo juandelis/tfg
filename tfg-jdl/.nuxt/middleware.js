8fc4438a5b5431bb8a9a3e55767ebef60eb7918a
Juan de Lis <juanlis96@hotmail.com>
2020-07-04 12:38:15 +0200

    limpieza archivos en git

--- tfg-jdl/.nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['autenticado'] = require('..\\middleware\\autenticado.js')
-middleware['autenticado'] = middleware['autenticado'].default || middleware['autenticado']
-
-export default middleware
